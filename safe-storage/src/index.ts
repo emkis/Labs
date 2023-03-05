13ee2807e00d499afd7e90da3bf048a0cb4920c9
emkis <nicolasemkis@gmail.com>
2023-03-05 19:50:59 +0000

    feat(safe-storage): trying API pt 2

--- safe-storage/src/index.ts
+++ safe-storage/src/index.ts
@@ -71,60 +71,32 @@ storage.getItem('theme', {
   validate: validateTheme,
 })
 
-// type GetItemOptions<T> = {
-//   validate?: (value: unknown) => asserts value is T
-// }
 
-type GetItemOptions<T> = T extends unknown
-  ? { validate?: (value: unknown) => asserts value is T }
-  : { validate?: (value: unknown) => T };
+type GetItemOptions<T> = {
+  validate?: (data: unknown) => T
+}
 
 function setItem<T>(key: string, options = {}) {
   return `${key}/dracula` as const
 }
 
-function getItem<T>(key: string, options: GetItemOptions<T> = {} as any): T {
+function getItem<T>(key: string, options: GetItemOptions<T> = {}): T {
+  const { validate } = options
   const data = JSON.parse(key)
-  options.validate && options.validate(data)
-  return data as T
+  return validate ? validate(data) : data
 }
-
-function gettItem<T>(key: string, options: { 
-  validate?: (value: unknown) => T
-} = {}): T {
-  const data = JSON.parse(key)
-  options.validate && options.validate(data)
-  return data as T
-}
-
 
 type Theme = { id: string }
-type User = { name: string, age: number }
+const transformTheme = (): Theme => ({ id: 'dark' })
 
 const Payment = z.object({
   id: z.string(),
   method: z.string(),
 });
 
-type Payment = z.infer<typeof Payment>;
+const one = getItem('one')
+const two = getItem('two', { validate: transformTheme })
+const three = getItem('three', { validate: Payment.parse })
+const four = getItem('four', { validate: Payment.safeParse })
 
-
-function assertTheme(value: unknown): asserts value is Theme {
-  if (typeof value !== 'object') {
-    throw new Error('is not a theme')
-  }
-}
-
-function assertUser(value: unknown): asserts value is User {
-  if (typeof value !== 'object') {
-    throw new Error('is not a theme')
-  }
-}
-
-
-export const theme = getItem('theme', { validate: assertTheme })
-export const user = getItem('user', { validate: assertUser })
-export const unknown = getItem('unknown')
-export const payment = getItem('payment', { validate: Payment.parse })
-
-const a = getItem<object>('a')
+four.success ? four.data.method : null
