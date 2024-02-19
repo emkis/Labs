2dd26c1aa879689944c4db1cf7f6d500f5f36561
emkis <nicolasemkis@gmail.com>
2024-02-19 23:53:44 +0000

    feat(type-safe-endpoints): work in progress

--- type-safe-endpoints/src/endpoint-url.ts
+++ type-safe-endpoints/src/endpoint-url.ts
@@ -1,23 +1,46 @@
-import type { Endpoints } from "./endpoints";
+/**
+ * In this file I'm trying to make the `endpointUrl` function to work with generic types.
+ * This way we have a default way of declaring the endpoint types and this will work on demand.
+ */
 
-/** Union type of all endpoint urls defined in the {@link Endpoints} type. */
-type Endpoint = keyof Endpoints;
+type EndpointUrl = string;
+type EndpointOptions = Record<string, string> | undefined;
+export type EndpointShape<
+  T extends Record<EndpointUrl, EndpointOptions> = Record<
+    EndpointUrl,
+    EndpointOptions
+  >
+> = T;
 
-/** Returns the options of a specific endpoint url. */
-type EndpointOptions<T extends Endpoint> = Endpoints[T];
+declare type MyEndpoints = EndpointShape<{
+  "/orders": undefined;
+  "/orders/{id}": { id: string };
+  "/foo/{bar}/fizz/{buzz}": { bar: string; buzz: string };
+}>;
 
 /** Returns the parsed endpoint url with given options. */
-export function endpointUrl<T extends Endpoint>(
-  url: T,
-  options?: EndpointOptions<T>
+export function endpointUrl<T extends EndpointShape>(
+  url: keyof T,
+  options?: T[typeof url]
 ): string {
+  // @ts-expect-error I don't know how to fix this
   let parsedUrl: string = url;
 
   for (const key in options) {
-    // @ts-expect-error Endpoint options are required, the keys will always exist
     const value: string = options[key];
     parsedUrl = parsedUrl.replace(`{${key}}`, value);
   }
 
   return parsedUrl;
 }
+
+// Playground
+// ----------
+
+endpointUrl("/user/{name}", { name: "John" }); // This should work by default with default types ✅
+
+endpointUrl<MyEndpoints>("/orders"); // This should work by default with default types ✅
+
+endpointUrl<MyEndpoints>("/orders/{id}"); // This should require the 2 arg ❌
+
+endpointUrl<MyEndpoints>("/foo/{bar}/fizz/{buzz}", {}); // This one should only infer its own options, not all of them ❌
---
+++ type-safe-endpoints/src/stable.ts
@@ -0,0 +1,39 @@
+/**
+ * This file is the stable version of the `endpointUrl` function.
+ *
+ * But it currently depends on the `Endpoints` type to work which should be created beforehand,
+ * I don't like this approach because it's not flexible enough, but it works for now
+ */
+
+import type { Endpoints } from "./endpoints";
+
+/** Union type of all endpoint urls defined in the {@link Endpoints} type. */
+type Endpoint = keyof Endpoints;
+
+/** Returns the options of a specific endpoint url. */
+type EndpointOptions<T extends Endpoint> = Endpoints[T];
+
+/** Returns the parsed endpoint url with given options. */
+export function endpointUrl<T extends Endpoint>(
+  url: T,
+  options?: EndpointOptions<T>
+): string {
+  let parsedUrl: string = url;
+
+  for (const key in options) {
+    // @ts-expect-error Endpoint options are required, the keys will always exist
+    const value: string = options[key];
+    parsedUrl = parsedUrl.replace(`{${key}}`, value);
+  }
+
+  return parsedUrl;
+}
+
+// Playground
+// ----------
+
+endpointUrl("/orders"); // this shouldn't require 2 arg ✅
+
+endpointUrl("/orders/{id}"); // this should require the 2 arg ❌
+
+endpointUrl("/orders/{id}", { id: "49fh" }); // this should infer only id ✅
