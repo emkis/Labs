b8c2df602b11f583a989a03744b3dea450a0f3aa
emkis <nicolasemkis@gmail.com>
2024-02-21 22:21:30 +0000

    feat(type-safe-endpoints): add solution to main file

--- type-safe-endpoints/src/endpoint-url.ts
+++ type-safe-endpoints/src/endpoint-url.ts
@@ -1,50 +1,13 @@
-/**
- * In this file I'm trying to make the `endpointUrl` function to work with generic types.
- * This way we have a default way of declaring the endpoint types and this will work on demand.
- */
+export type EndpointUrl = string;
+export type EndpointOptions = Record<string, string> | undefined;
+export type EndpointObject = Record<EndpointUrl, EndpointOptions>;
+export type CreateEndpointObject<T extends EndpointObject> = T;
 
-type EndpointUrl = string;
-type EndpointOptions = Record<string, string> | undefined;
-export type EndpointShape<
-  T extends Record<EndpointUrl, EndpointOptions> = Record<
-    EndpointUrl,
-    EndpointOptions
-  >
-> = T;
-
-declare type MyEndpoints = EndpointShape<{
-  "/orders": undefined;
-  "/orders/{id}": { id: string };
-  "/foo/{bar}/fizz/{buzz}": { bar: string; buzz: string };
-}>;
-
-/** Returns the parsed endpoint url with given options. */
-export function endpointUrl<T extends EndpointShape>(
-  url: keyof T,
-  options?: T[typeof url]
-): string {
-  // @ts-expect-error I don't know how to fix this
-  let parsedUrl: string = url;
-
-  for (const key in options) {
-    const value: string = options[key];
-    parsedUrl = parsedUrl.replace(`{${key}}`, value);
-  }
-
-  return parsedUrl;
+export function createEndpointUrlParser<T extends EndpointObject>() {
+  return function endpointUrlParser<Url extends keyof T>(
+    url: Url,
+    options?: T[Url]
+  ): string {
+    return "";
+  };
 }
-
-// Playground
-// ----------
-
-endpointUrl("/users"); // Should work by default with no narrow types as generic ✅
-endpointUrl("/users", undefined); // Should work by default with no narrow types as generic ✅
-endpointUrl("/user/{name}", { name: "John" }); // Should work by default with no narrow types as generic ✅
-
-endpointUrl<MyEndpoints>("/orders"); // Should work with narrow generic type and no 2nd arg ✅
-
-endpointUrl<MyEndpoints>("/orders/{id}"); // Should require the 2 arg ❌
-
-endpointUrl<MyEndpoints>("/foo/{bar}/fizz/{buzz}", {}); // Should only infer its own options, not all of them ❌
-
-endpointUrl<MyEndpoints>("/foo/{bar}/fizz/{buzz}", { id: "3" }); // Should not allow `id` ❌
