b52cdc237e6bf56e0d4393a71a921bd70ab2c6d5
emkis <nicolasemkis@gmail.com>
2024-02-24 23:38:53 +0000

    (type-safe-endpoints): add development version of url parser

---
+++ type-safe-endpoints/src/url-parser.development.ts
@@ -0,0 +1,53 @@
+export function urlParser(
+  url: string,
+  urlValues: Record<string, string> | undefined
+): string {
+  const requiredUrlValues = getUrlValues(url);
+  if (!requiredUrlValues) return url;
+
+  if (!urlValues || Object.keys(urlValues).length === 0) {
+    throw new Error(
+      `${urlParser.name}: The values for "${url}" URL are required.`
+    );
+  }
+
+  let parsedUrl: string = url;
+  const replacedValues: string[] = [];
+
+  for (const key in urlValues) {
+    const isRequiredKey = requiredUrlValues.includes(key);
+    const optionValue = urlValues[key];
+    if (!isRequiredKey || !optionValue) continue;
+
+    parsedUrl = parsedUrl.replaceAll(`{${key}}`, optionValue);
+    replacedValues.push(key);
+  }
+
+  if (replacedValues.length !== requiredUrlValues.length) {
+    const missingKeys = getUrlValues(parsedUrl)!;
+    const formattedMissingKeys = missingKeys.join(", ");
+
+    throw new Error(
+      `${urlParser.name}: The values for "${url}" URL are required. Missing keys are: ${formattedMissingKeys}.`
+    );
+  }
+
+  return parsedUrl;
+}
+
+/**
+ * Returns the dynamic values from a url.
+ *
+ * @example
+ * getUrlValues("/orders") // undefined
+ * getUrlValues("/orders/{id}") // ["id"]
+ * getUrlValues("/orders/{id}/cancel/{id}") // ["id"]
+ * getUrlValues("/orders/{id}/foo/{t_id}") // ["id", "t_id"]
+ */
+function getUrlValues(url: string): string[] | undefined {
+  const matches = url.match(/{(.*?)}/g);
+  if (!matches) return;
+
+  const uniqueMatches = Array.from(new Set(matches));
+  return uniqueMatches;
+}
