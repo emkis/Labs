7c6444a82df62e11b2a28135d1b774885404e2b4
emkis <nicolasemkis@gmail.com>
2024-03-14 18:16:34 +0000

    (api-endpoint-centraliser): updating type tests

--- api-endpoint-centraliser/src/endpoint-url.test-d.ts
+++ api-endpoint-centraliser/src/endpoint-url.test-d.ts
@@ -13,21 +13,20 @@ const endpointUrl = createEndpointUrlParser<Endpoints>();
 
 it("should always return a string type", () => {
   assertType<string>(endpointUrl("/users"));
-  assertType<string>(endpointUrl("/payments", undefined));
   assertType<string>(endpointUrl("/payments/{id}", { id: "3" }));
 });
 
 it("should not error when correct options of endpoint are provided", () => {
   endpointUrl("/payments");
-  endpointUrl("/payments", undefined);
   endpointUrl("/payments/{id}", { id: "3" });
   endpointUrl("/users");
-  endpointUrl("/users", undefined);
   endpointUrl("/users/{user_name}", { user_name: "emkis" });
   endpointUrl("/payments/{p_id}/transaction/{tr_id}", { p_id: "hjs", tr_id: "92z" });
 });
 
 it("should error when options of endpoint are invalid, missing or partially provided", () => {
+  // @ts-expect-error
+  endpointUrl("/payments", undefined);
   // @ts-expect-error
   endpointUrl("/payments", {});
   // @ts-expect-error
